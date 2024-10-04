b073099e9f4e52efc8a5b0266d378c7ecbca127e
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2024-10-04 21:43:47 +0300

    Add AuthorizeView sample app

---
+++ AuthorizeView/T1666359/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace T1666359.Services
+{
+	public class CustomAuthStateProvider : AuthenticationStateProvider
+	{
+        private ClaimsPrincipal _currentUser { get; set; } = new(new ClaimsIdentity());
+    
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(_currentUser));
+        }
+
+        public void SignIn(string userIdentifier)
+        {
+            var claims = new Claim[] { new Claim(ClaimTypes.Name, userIdentifier) };
+
+            var identity = new ClaimsIdentity(claims, "Custom Authentication");
+
+            _currentUser = new ClaimsPrincipal(identity);
+        
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_currentUser)));
+        }
+
+        public void SignOut()
+        {
+            _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_currentUser)));
+        }
+    }
+}
