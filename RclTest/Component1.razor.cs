ff703b6e982d5b5828f2597493a2ab6489fff85a
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2024-10-11 21:30:55 +0300

    Add RCL sample project

---
+++ RclTest/Component1.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace RclTest.Components
+{
+	public partial class Component1 : ComponentBase
+	{
+        [Parameter]
+        public string Value { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        private async Task OnInput(ChangeEventArgs args)
+        {
+            Value = args.Value?.ToString() ?? string.Empty;
+
+            if (ValueChanged.HasDelegate)
+            {
+                await ValueChanged.InvokeAsync(Value);
+            }
+        }
+
+    }
+}
