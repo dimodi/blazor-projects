cf6c1290d6d639f1b7d6bf0e08f5b12b678e0f01
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2024-10-18 22:16:43 +0300

    Upgrade .NET version in TelerikRazorTemplates

---
+++ TelerikRazorTemplates/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.SignalR;
+using TelerikRazorTemplates.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddTelerikBlazor();
+
+// Upload file size limit on ASP.NET Core
+builder.Services.Configure<FormOptions>(options =>
+{
+    //options.MultipartBodyLengthLimit = 4_294_967_296; // 4 GB
+});
+// Upload file size limit on Kestrel
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    //options.Limits.MaxRequestBodySize = 4_294_967_296; // 4 GB
+});
+
+// SignalR max message size for Editor, FileManager, FileSelect, PDF Viewer, Signature
+builder.Services.Configure<HubOptions>(options =>
+{
+    //options.MaximumReceiveMessageSize = 64 * 1024 * 1024; // 64 MB or use null for no limit
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
