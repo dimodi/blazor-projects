3a075dd39367dbda6a0e2527b6dcfb7a14ed4699
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2025-02-25 11:43:01 +0200

    Update OData Sample

--- ODataSample/ClassLibrary/SampleModel.cs
+++ ODataSample/ClassLibrary/SampleModel.cs
@@ -4,6 +4,7 @@ public class SampleModel
 {
     public int Id { get; set; }
     public string StringProperty { get; set; } = string.Empty;
+    public char CharProperty { get; set; }
     public int IntProperty { get; set; }
     public decimal DecimalProperty { get; set; }
     public DateTime DateTimeProperty { get; set; }
